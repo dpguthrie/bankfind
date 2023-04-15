e9cccc7e8bbe215ae990c7f9e2c3b273cea7b443
Ross <rdavisson@gmail.com>
2023-04-15 15:29:10 -0700

    modifications to enable financials api from bankfind

--- bankfind/metadata/__init__.py
+++ bankfind/metadata/__init__.py
@@ -3,6 +3,7 @@ from .history import history_dict
 from .institution import institution_dict
 from .location import location_dict
 from .summary import summary_dict
+from .financials import financials_dict
 
 
 meta_dict = {
@@ -10,5 +11,6 @@ meta_dict = {
     'history': history_dict,
     'institutions': institution_dict,
     'locations': location_dict,
-    'summary': summary_dict
+    'summary': summary_dict,
+    'financials': financials_dict
 }
